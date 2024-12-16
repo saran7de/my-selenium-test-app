62044cfed6f69b001f3935c497ed45a96abd912e
saran7de_git <saran7de@gmail.com>
2024-12-15 22:04:39 -0600

    App upd1

--- src/main/java/org/test/my_app_test/App.java
+++ src/main/java/org/test/my_app_test/App.java
@@ -27,7 +27,8 @@ public class App {
 
 		// 1. Open the URL
 		// driver.get("http://13.58.166.105:8080/addressbook-2.0/");
-		driver.get("http://3.23.105.112:8081/contact.html");
+		//driver.get("http://3.23.105.112:8081/contact.html");
+		driver.get("http://localhost:8082/contact.html");
 		// invoke implicit wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
 
